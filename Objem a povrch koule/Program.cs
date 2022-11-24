25e93e50b608d7c374a15b142d732829d5144fd1
drozboril <rozborildavid77@gmail.com>
2022-11-24 17:43:25 +0100

    Uživatel zadá poloměr koule na jehož základě program vypočítá povrch a objem

---
+++ Objem a povrch koule/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objem_a_povrch_koule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r, povrch, objem;
+            double PI = 3.14;
+            Console.WriteLine("Zadejte poloměr koule");
+            r = Convert.ToDouble(Console.ReadLine());
+            povrch = 4 * PI * r * r;
+            objem = (4.0 / 3) * PI * r * r * r;
+            Console.WriteLine("Povrch koule je {0} ", povrch);
+            Console.WriteLine("Objem koule je {0}", objem);
+            Console.Read();
+        }
+    }
+}
